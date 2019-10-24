d0848a9247cf379cf5ae0b65c290408ecde6a84d
Aravind <aravind4532@gmail.com>
2019-10-24 12:02:41 +0530

    Second Commit 1)Turn on/off bluetooth 2)Bluetooth adapter class used 3)Broadcast receiver for state change

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +26,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
 
       turnon.setOnClickListener(this);
+
+
+    }
+ BroadcastReceiver broadcastReceiver=new BroadcastReceiver() {
+     @Override
+     public void onReceive(Context context, Intent intent) {
+         String action=intent.getAction();
+         if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action))
+         {
+             int state=intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,BluetoothAdapter.ERROR);
+             switch (state)
+             {
+                 case BluetoothAdapter.STATE_ON:
+                     Toast.makeText(context, "Turned on", Toast.LENGTH_SHORT).show();
+                     break;
+                 case BluetoothAdapter.STATE_TURNING_ON:
+                     Toast.makeText(context, "Turning on", Toast.LENGTH_SHORT).show();
+                     break;
+                 case BluetoothAdapter.STATE_TURNING_OFF:
+                     Toast.makeText(context, "Turning off", Toast.LENGTH_SHORT).show();
+                     break;
+                 case BluetoothAdapter.STATE_OFF:
+                     Toast.makeText(context, "Turned off in broadcast", Toast.LENGTH_SHORT).show();
+                     break;
+             }
+         }
+     }
+ };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intent=new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(broadcastReceiver,intent);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
     }
 
     @Override
